f75294bf561894675955741b9687d34a46d5dec4
Alexey Semerenko <alexey@semerenko.msk.ru>
2019-09-29 17:37:21 +0300

    init project

---
+++ el-file-proxy.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: El File proxy
+Plugin URI: http://github.com/lexasem
+Description: this plugin substitutes file urls
+Author: Alexey Semerenko
+Version: 1.0.0
+Author http://github.com/lexasem
+*/
