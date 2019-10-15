d110a6676e0303f1ad77e5b1468dd7418d850e41
Alexey Semerenko <alexey@semerenko.msk.ru>
2019-10-15 19:43:40 +0300

    replace file get contents with curl

--- el-file-proxy/includes/ElFileProxy.php
+++ el-file-proxy/includes/ElFileProxy.php
@@ -129,7 +129,14 @@ class ElFileProxy
 
     private function loadFileContents($url)
     {
-        return @file_get_contents($url);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        $data = curl_exec($curl);
+        curl_close($curl);
+
+        return $data;
     }
 
     private function exportFile($contents, $mimeType, $fileName)
